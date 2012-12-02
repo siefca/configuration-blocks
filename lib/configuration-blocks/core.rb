379849ff5570306b892db0b94923cc0d385658df
Paweł Wilk <siefca@gnu.org>
2012-12-02 02:48:40 +0100

    Anonymous modules caching fixed, added singleton support

--- lib/configuration-blocks/core.rb
+++ lib/configuration-blocks/core.rb
@@ -12,16 +12,8 @@ module ConfigurationBlocks
   # @private
   def self.included(base)
     base.extend(ClassMethods)
-  end
-
-  # Returns configuration module.
-  # 
-  # @param base [Object,Symbol] base object which delegators will point to (defaults to object on which
-  #  this method has been called). If symbol is given, then it should contain the name of a method that
-  #  will be called on current object.
-  # @return [Module] anonymous module with proxy module methods delegating actions to +base+ object
-  def configuration_module(base = self)
-    self.class.configuration_module(base)
+    base.singleton_class.extend(ClassMethods)
+    super if defined?(super)
   end
 
   # Returns configuration block.
@@ -34,29 +26,66 @@ module ConfigurationBlocks
     self.class.configuration_block(base, &block)
   end
 
-  # Gets the duplicate of all the delegated method names from current class or module
-  # and from all modules included that implement {ConfigurationBlocks}.
+  # Returns configuration module that delegates methods pointing to the given +base+ object
+  # and optionally evaluates the given block in the context of this module or in the current context.
   # 
-  # @param local_only [Boolean] optional flag that if set, causes only methods added by current class or module
-  #  to be listed. (defaults to +false+)
-  # @return [Array<Symbol>] delegated method names
-  def configuration_delegated_methods(local_only = false)
-    self.class.configuration_delegated_methods(local_only)
+  # If a block is given then it is evaluated in the context of the configuration module.
+  # If a block with at least one argument is given then the context remains as it was
+  # but the first argument is the module.
+  # 
+  # @example
+  #   # using external access to configuration module:
+  #   x.configuration_block do |c|
+  #     c.some_setting  :a
+  #     c.other_setting :b
+  #   end
+  #   
+  #   # using internal access to configuration module:
+  #   x.configuration_block do
+  #     some_setting  :a
+  #     other_setting :b
+  #   end
+  # 
+  # @param base [Object,Symbol] base object which delegators will point to (defaults to object on which
+  #  this method has been called). If symbol is given, then it should contain the name of a method that
+  #  will be called on current object.
+  # @return [Module,Object] configuration module if not block is given or a block takes no arguments. In case
+  #  of one or more arguments in a block it also returns a module unless +break+ statement is used; in such case
+  #  the value passed to +break+ is returned.
+  def configuration_block(base = self, &block)
+    @cb_conf_module ||= self.class.configuration_module(base)
+    self.class.send(:configuration_block_core, @cb_conf_module, &block)
   end
+
+  private
 
   # This module contains methods that will extend the class.
   module ClassMethods
 
-    # This method creates and returns anonymous module containing
-    # delegators that point to methods from a class this module is included in.
+    # Returns configuration block.
     # 
     # @param base [Object,Symbol] base object which delegators will point to (defaults to object on which
     #  this method has been called). If symbol is given, then it should contain the name of a method that
     #  will be called on current object.
+    # 
+    # @return [Object] result of evaluating the given block within context of configuration module
+    def configuration_block(base = self, &block)
+      @cb_conf_module ||= configuration_module(base)
+      configuration_block_core(@cb_conf_module, &block)
+    end
+
+    # Creates and returns anonymous module containing
+    # delegators that point to methods from a class this module is included in
+    # or the given +base+.
+    # 
+    # @param base [Object,Symbol] base object which delegators will point to (defaults to object on which
+    #  this method has been called). If symbol is given, then it should contain the name of a method that
+    #  will be called on current object.
+    # 
     # @return [Module] anonymous module with proxy module methods delegating actions to +base+ object
     def configuration_module(base = self)
-      @cb_conf_module ||= Module.new.tap do |cm|
-        delegators = configuration_delegated_methods
+      Module.new.tap do |cm|
+        delegators = get_configuration_methods
         base = send(base) if base.is_a?(Symbol)
         cm.extend Module.new {
           delegators.each do |method|
@@ -70,43 +99,22 @@ module ConfigurationBlocks
       end
     end
 
-    # Returns configuration module that delegates methods pointing to the given +base+ object
-    # and optionally evaluates the given block in the context of this module or in the current context.
+    # Gets all method names known to configuration engine.
     # 
-    # If a block is given then it is evaluated in the context of the configuration module.
-    # If a block with at least one argument is given then the context remains as it was
-    # but the first argument is the module.
+    # @param local_only [Boolean] optional flag that if set, causes only methods added
+    #  by current class or module to be listed. (defaults to +false+)
     # 
-    # @example
-    #   # using external access to configuration module:
-    #   x.configuration_block do |c|
-    #     c.some_setting  :a
-    #     c.other_setting :b
-    #   end
-    #   
-    #   # using internal access to configuration module:
-    #   x.configuration_block do
-    #     some_setting  :a
-    #     other_setting :b
-    #   end
-    # 
-    # @param base [Object,Symbol] base object which delegators will point to (defaults to object on which
-    #  this method has been called). If symbol is given, then it should contain the name of a method that
-    #  will be called on current object.
-    # @return [Module,Object] configuration module if not block is given or a block takes no arguments. In case
-    #  of one or more arguments in a block it also returns a module unless +break+ statement is used; in such case
-    #  the value passed to +break+ is returned.
-    def configuration_block(base = self, &block)
-      cm = configuration_module(base)
-      return cm unless block_given?
-      return cm.tap(&block) unless block.arity == 0
-      cm.module_eval(&block)
-      cm
+    # @return [Array<Symbol>] delegated method names
+    def get_configuration_methods(local_only = false)
+      all_delegators = singleton_class.send(:cf_block_delegators) + cf_block_delegators
+      return all_delegators if local_only
+      ancestors.each_with_object(all_delegators) do |ancestor, all|
+        all.merge(ancestor.send(__method__, true)) if ancestor.respond_to?(__method__)
+      end
     end
 
-    # This DSL method is intended to be used in a class to indicate which methods
-    # should be delegated from anonymous proxy module available when {configuration_block}
-    # or {configuration_module} method is called.
+    # This DSL method is intended to be used in a class or module to indicate which methods
+    # should be delegated.
     # 
     # @param methods [Array<Symbol,String>] list of method names
     # @return [nil]
@@ -119,20 +127,6 @@ module ConfigurationBlocks
     alias_method :configuration_methods,  :configuration_block_delegate
     alias_method :settings_method,        :configuration_block_delegate
 
-    # Gets the duplicate of all the delegated method names from current class or module
-    # and from all ancestors or a current object that implement {ConfigurationBlocks}.
-    # 
-    # @param local_only [Boolean] optional flag that if set, causes only methods added
-    #  by current class or module to be listed. (defaults to +false+)
-    # @return [Array<Symbol>] delegated method names
-    def configuration_delegated_methods(local_only = false)
-      all_delegators = cf_block_delegators.dup
-      return all_delegators if local_only
-      ancestors.each_with_object(all_delegators) do |ancestor, all|
-        all.merge(ancestor.send(__method__, true)) if ancestor.respond_to?(__method__)
-      end
-    end
-
     private
 
     # Getter for +@cf_block_delegators+ internal variable.
@@ -140,7 +134,14 @@ module ConfigurationBlocks
       @cf_block_delegators ||= Set.new
     end
 
+    # Evaluates configuration block within a context of the given module.
+    def configuration_block_core(conf_module, &block)
+      return conf_module unless block_given?
+      return conf_module.tap(&block) unless block.arity == 0
+      conf_module.module_eval(&block)
+      conf_module
+    end
+
   end # module ClassMethods
 
 end # module ConfigurationBlocks
-
